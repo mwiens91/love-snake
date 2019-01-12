52a295ad8733d7596d5fceda7be608cf13edd3b5
Matt Wiens <mwiens91@gmail.com>
2019-01-11 18:50:38 -0800

    Add spooky background music

--- main.lua
+++ main.lua
@@ -12,6 +12,11 @@ function love.load()
   }
 
   love.window.setMode(game_width * 2, game_height * 2, window_flags)
+
+  -- Add background music
+  music = love.audio.newSource("assets/background_music.wav", "stream")
+  music:setLooping(true)
+  music:play()
 end
 
 function love.draw()
