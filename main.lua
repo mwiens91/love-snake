68dcc89a84f9c51e33bcca220bd3a953d0d787bb
Matt Wiens <mwiens91@gmail.com>
2019-01-11 17:46:27 -0800

    Center game area in window

--- main.lua
+++ main.lua
@@ -6,14 +6,12 @@ function love.load()
 
   -- Set the window
   local window_flags = {
-    centered=true,
     minheight=game_height,
     minwidth=game_width,
     resizable=true,
   }
 
   love.window.setMode(game_width * 2, game_height * 2, window_flags)
-
 end
 
 function love.draw()
@@ -24,15 +22,24 @@ function love.draw()
   -- Scale to window while maintaining aspect ratio
   local scale_width
   local scale_height
+  local translate_x
+  local translate_y
 
   if window_width >= aspect_ratio * window_height then
     scale_height = window_height / game_height
     scale_width = scale_height
+
+    translate_x = (window_width - game_width * scale_width) / 2
+    translate_y = 0
   else
     scale_width = window_width / game_width
     scale_height = scale_width
+
+    translate_x = 0
+    translate_y = (window_height - game_height * scale_height) / 2
   end
 
+  love.graphics.translate(translate_x, translate_y)
   love.graphics.scale(scale_width, scale_height)
 
   love.graphics.printf("hello", 0, game_height / 2, game_width, "center")
