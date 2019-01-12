086f1e3e9566127b790e44b5040eebf7cdaea2b1
Matt Wiens <mwiens91@gmail.com>
2019-01-11 17:17:51 -0800

    Setup window attrs and scaling

--- main.lua
+++ main.lua
@@ -1,3 +1,40 @@
+function love.load()
+  -- Minimum dimensions of game
+  game_width = 400
+  game_height = 300
+  aspect_ratio = game_width / game_height
+
+  -- Set the window
+  local window_flags = {
+    borderless=true,
+    centered=true,
+    minheight=game_height,
+    minwidth=game_width,
+    resizable=true,
+  }
+
+  love.window.setMode(game_height, game_width, window_flags)
+
+end
+
 function love.draw()
-  love.graphics.print("Hey", 400, 300)
+  -- Dimensions of current window
+  local window_width = love.graphics.getWidth()
+  local window_height = love.graphics.getHeight()
+
+  -- Scale to window while maintaining aspect ratio
+  local scale_width
+  local scale_height
+
+  if window_width >= aspect_ratio * window_height then
+    scale_height = window_height / game_height
+    scale_width = scale_height
+  else
+    scale_width = window_width / game_width
+    scale_height = scale_width
+  end
+
+  love.graphics.scale(scale_width, scale_height)
+
+  love.graphics.printf("hello", 0, game_height / 2, game_width, "center")
 end
